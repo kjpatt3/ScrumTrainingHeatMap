dcc82e408cc6d3cf424dc31405dd651e6b6cbf90
KJ Patterson <kj.patterson@aggiemail.usu.edu>
2020-10-30 15:49:09 -0600

    X Axis now has colors to match the questions.

---
+++ xAxis.py
@@ -0,0 +1,18 @@
+
+class Axis:
+    def init(self):
+        self.greenX = [0,1,1,1]
+        self.greenY = [0.3,0.3,0,0.3]
+        self.yellowX = [1,2,2,2]
+        self.yellowY = [0.3,0.3,0,0.3]
+        self.orangeX = [2,3,3,3]
+        self.orangeY = [0.3,0.3,0,0.3]
+        self.orangeX2 = [3,4,4,4]
+        self.orangeY2 = [0.3,0.3,0,0.3]
+        self.orangeX3 = [4,5,5,5]
+        self.orangeY3 = [0.3,0.3,0,0.3]
+        self.redX = [5,6,6,6]
+        self.redY = [0.3,0.3,0,0.3]
+
+
+
